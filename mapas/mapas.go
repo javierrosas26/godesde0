6193297396b1d2d3fdf7d86664553f026924f74f
javier <javier9706@gmail.com>
2023-06-29 16:40:22 +0000

    Siguiente Commit

---
+++ mapas/mapas.go
@@ -0,0 +1,34 @@
+package mapas
+
+import "fmt"
+
+func MostrarMapas() {
+	paises := make(map[string]string)
+	//fmt.Println(paises)
+
+	paises["Mexico"] = "D.F."
+	paises["Argentina"] = "Buenos Aires"
+
+	/*fmt.Println(paises)
+	fmt.Println(paises["Argentina"])*/
+
+	campeonato := map[string]int{
+		"Barcelona":   39,
+		"Real Madrid": 38,
+		"Chivas":      37,
+		"River":       30,
+	}
+
+	fmt.Println(campeonato)
+
+	/*for equipo, puntaje := range campeonato {
+		fmt.Printf("Equipo %s, tiene un puntaje de %d \n", equipo, puntaje)
+	}*/
+
+	delete(campeonato, "Chivas")
+	fmt.Println(campeonato)
+
+	puntaje, existe := campeonato["Real Madrid"]
+	fmt.Printf("El puntaje capturado es %d, y el equipo existe = %t \n", puntaje, existe)
+
+}
